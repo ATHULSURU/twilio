a16cf5fc0e703f61bcf639d3ea5fa6fd8774e8eb
Athul <athul@regalix-inc.com>
2019-07-22 19:01:35 +0530

    Auto Call Answering

--- src/Listeners.js
+++ src/Listeners.js
@@ -1,7 +1,11 @@
-import { Actions, Manager } from '@twilio/flex-ui';
+import { Actions, Manager } from "@twilio/flex-ui";
 
 export const setListeners = () => {
-
+  Manager.getInstance().workerClient.on("reservationCreated", reservation => {
+    if (reservation.task.taskChannelUniqueName === "voice") {
+      Actions.invokeAction("AcceptTask", { sid: reservation.sid });
+    }
+  });
 };
 
 export default setListeners;
