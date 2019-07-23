d0f46d71ee756c24ed49597eeb13311bc3eda6de
Athul <athul@regalix-inc.com>
2019-07-23 11:29:09 +0530

    Auto Call Answering after correction

--- src/Listeners.js
+++ src/Listeners.js
@@ -2,9 +2,7 @@ import { Actions, Manager } from "@twilio/flex-ui";
 
 export const setListeners = () => {
   Manager.getInstance().workerClient.on("reservationCreated", reservation => {
-    if (reservation.task.taskChannelUniqueName === "voice") {
-      Actions.invokeAction("AcceptTask", { sid: reservation.sid });
-    }
+    Actions.invokeAction("AcceptTask", { sid: reservation.sid });
   });
 };
 
